35d50ffb89a7b40790e562254e49f75af2d1fcb7
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2023-11-15 11:38:01 -0800

    Add html + Business logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+//Business Logic
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './css/styles.css';
+
+function getDrinks(searchInput) {
+	let search = new XMLHttpRequest();
+	const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita&appid=1'
+	
+	request.addEventListener("loadend", function () {
+		const response = JSON.parse(this.responseText);
+		if (this.status === 200) {
+			printElemtents(response, searchInput);
+		} else {
+			printError(this, responsem, searchInput);
+		}
+	});
+	
+	request.open("GET", url, true);
+	request.send();
+}
+
+
+// UI Logic
