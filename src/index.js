4a5f95aae373369edd73a0097653323e0e609a3a
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2023-11-15 13:39:33 -0800

    Add UI logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 //Business Logic
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './css/styles.css';
 
 function getDrinks(searchInput) {
 	let search = new XMLHttpRequest();
-	const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita&appid=1'
+	const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
 	
 	request.addEventListener("loadend", function () {
 		const response = JSON.parse(this.responseText);
@@ -23,3 +22,34 @@ function getDrinks(searchInput) {
 
 
 // UI Logic
+function printError(request, apiResponse, searchInput) {
+	document.querySelector('#returnDrink').innerText = 'There was an error finding drinks using the word ${searchInput}: ${request.status} ${request.status.text}: ${apiResponse.message}';
+}
+
+function printElemtents(apiResponse) {
+	const drinkDiv = document.querySelector('#returnDrink');
+	const drink = document.createElement('img');
+
+drinkDiv.innerText = "";
+
+try {
+	drink.src = apiResponse.data[1].images.original.url;
+	drink.alt = "a drink recipe";
+	drinkDiv.append(drink);
+} catch (error) {
+	console.error("Error accessing cocktail URL:", error);
+	drink.alt = "error loading drink";
+	drinkDiv.append(drink);
+	}
+}
+
+function handleFormSubmission(event) {
+	event.preventDefault();
+	const searchInput = document.querySelector('#drink').value; 
+	document.querySelector('#drink').value = null;
+	getDrinks(searchInput);
+}
+
+window.addEventListener("load", function () {
+	this.document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
